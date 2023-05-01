db84b1f63a941254c0f99ac670048af59d6dabb8
TristanHoffay <38638607+TristanHoffay@users.noreply.github.com>
2023-04-30 21:03:04 -0700

    done

--- Assets/Parallax.cs
+++ Assets/Parallax.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class Parallax : MonoBehaviour
 {
+    [SerializeField]
+    private float[] parallaxFactors = new float[10];
+
+    private SpriteRenderer[] backgrounds;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        backgrounds = transform.GetComponentsInChildren<SpriteRenderer>();
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Vector3 cameraPos = Camera.main.transform.position;
+        cameraPos.z = 0;
+        for (int i = 0; i < backgrounds.Length; i++)
+        {
+            backgrounds[i].transform.localPosition = cameraPos / parallaxFactors[i];  // the larger the factor, closer to camera
+        }
     }
 }
